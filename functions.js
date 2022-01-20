d0ee3c8be9ff6293cc05ef78af2f960c4e43db7b
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-20 14:55:53 +0300

    functions updated

--- functions.js
+++ functions.js
@@ -10,3 +10,25 @@ function square(number) {
 }
 let result = square(3);
 console.log(result);
+
+
+let circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() { console.log("drawing...")}
+};
+circle.draw();
+
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw(){console.log("drawing...")}
+    }
+}
+let circle2 = createCircle(2);
+circle2.draw();
