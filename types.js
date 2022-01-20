1304b75bcd78a7aac3b48a2bf9657bfde883221c
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-20 09:50:46 +0300

    file renamed

---
+++ types.js
@@ -0,0 +1,12 @@
+console.log("hi there!");
+
+let name = "James";
+console.log(typeof name);
+
+// value undefined and type undefined.
+let number = undefined;
+console.log(typeof number); // undefined    
+
+// null value is of type 'object'.
+let product = null;
+console.log(typeof product); // object
