468d779f2b74fa616902354275400da8af13ffcf
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-20 10:26:20 +0300

    types updated

--- types.js
+++ types.js
@@ -10,3 +10,17 @@ console.log(typeof number); // undefined
 // null value is of type 'object'.
 let product = null;
 console.log(typeof product); // object
+
+let person = {
+    name: "john",
+    age: 34
+}
+// two ways to access object attributes
+console.log(person.name, typeof person);
+console.log(person["age"]);
+
+// arrays: object type
+let myList = ["Merry", 23];
+console.log(myList, typeof myList);
+myList[2] = true;
+console.log(myList);
