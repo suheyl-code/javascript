9ae664086306cb78d956e09a63401aed0e4a6068
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-21 09:32:32 +0300

    fie added

---
+++ chessboard.js
@@ -0,0 +1,9 @@
+// Exercise p. 39 of 'Eloquent Javascript'
+for(let i = 0; i < 8; i++) {
+    if (i % 2 == 0) {
+        console.log(' # # # #');
+    }
+    else {
+        console.log('# # # # ');
+    }
+}
