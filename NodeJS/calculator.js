514df216fe19e5668b6f5fb58f0819217a7e868a
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-24 10:51:07 +0300

    .

--- NodeJS/calculator.js
+++ NodeJS/calculator.js
@@ -14,6 +14,13 @@ function division(a,b) {
   return a / b;
 }
 
-module.exports.addition = addition;
-module.exports.subtraction = subtraction;
+//module.exports.addition = addition;
+//module.exports.subtraction = subtraction;
+// or like the following syntax:
+module.exports = {
+  addition: addition,
+  subtraction: subtraction,
+  multiplication: multiplication,
+  division: division,
+}
 //console.log(module);
