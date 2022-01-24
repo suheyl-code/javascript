54e30a5a54e41db0d165838c9ba0ebc8f4933034
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-24 18:40:33 +0300

    one more innerfunction added

--- innerfunctions.js
+++ innerfunctions.js
@@ -1,17 +1,29 @@
 // gives an error:
 // Uncaught RangeError: Maximum call stack size exceeded
-function chicken() {
-    return egg();
+// function chicken() {
+//     return egg();
+// }
+
+// function egg() {
+//     return chicken();
+// }
+
+// console.log(chicken() + " came first ");
+
+// // another version with same error
+// const chicken2 = () => egg2();
+// const egg2 = () => chicken2();
+
+// console.log(chicken2());
+
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
+    }
 }
 
-function egg() {
-    return chicken();
-}
-
-console.log(chicken() + " came first ");
-
-// another version with same error
-const chicken2 = () => egg2();
-const egg2 = () => chicken2();
-
-console.log(chicken2());
+let items = [];
+repeat(5, i => {
+    items.push(`this is ${i}`);
+})
+console.log(items);
