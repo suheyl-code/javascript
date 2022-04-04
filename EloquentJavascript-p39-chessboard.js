10f31f675894b8f38e98d5c370056a3c7bcab43d
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-04-04 15:20:00 +0300

    new practice add

--- EloquentJavascript-p39-chessboard.js
+++ EloquentJavascript-p39-chessboard.js
@@ -1,5 +1,19 @@
 // Exercise p. 39 of 'Eloquent Javascript'
-for(let i = 0; i < 8; i++) {
+/*
+Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space
+or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+*/
+for (let i = 0; i < 8; i++) {
     if (i % 2 == 0) {
         console.log(' # # # #');
     }
